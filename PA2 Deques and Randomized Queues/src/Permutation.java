435f796c6168342a0a6c3d64c261085743ed36f4
carrordeng <carror1994@gmail.com>
2017-09-16 21:18:28 +0800

    primitive

---
+++ PA2 Deques and Randomized Queues/src/Permutation.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int K=0;
+        if(args.length>=1)
+            K=Integer.parseInt(args[0]);
+    }
+}
